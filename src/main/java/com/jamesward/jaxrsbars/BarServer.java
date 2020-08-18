3dd06b5259e0658e5cc980182617ab58be4ec8c4
James Ward <james@jamesward.com>
2020-08-18 15:21:28 -0600

    to objectrocket mongo

--- src/main/java/com/jamesward/jaxrsbars/BarServer.java
+++ src/main/java/com/jamesward/jaxrsbars/BarServer.java
@@ -18,19 +18,19 @@ import javax.ws.rs.core.UriBuilder;
 public class BarServer {
 
     public static DB mongoDB;
-    
+
     public static String contentUrl;
 
     private static final String CONTENT_PATH = "/content";
 
     public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
-        final int port = System.getenv("PORT") != null ? Integer.valueOf(System.getenv("PORT")) : 8080;
+        final int port = System.getenv("PORT") != null ? Integer.parseInt(System.getenv("PORT")) : 8080;
         final URI baseUri = UriBuilder.fromUri("http://0.0.0.0/").port(port).build();
         final Application application = Application.builder(ResourceConfig.builder().packages(BarServer.class.getPackage().getName()).build()).build();
         application.addModules(new JsonJacksonModule());
         final HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(baseUri, application);
         httpServer.getServerConfiguration().addHttpHandler(new StaticHttpHandler("src/main/webapp"), CONTENT_PATH);
-        
+
         for (NetworkListener networkListener : httpServer.getListeners()) {
             if (System.getenv("FILE_CACHE_ENABLED") == null) {
                 networkListener.getFileCache().setEnabled(false);
@@ -44,11 +44,17 @@ public class BarServer {
             }
         });
 
-        MongoURI mongolabUri = new MongoURI(System.getenv("MONGOLAB_URI") != null ? System.getenv("MONGOLAB_URI") : "mongodb://127.0.0.1:27017/hello");
-        Mongo m = new Mongo(mongolabUri);
-        mongoDB = m.getDB(mongolabUri.getDatabase());
-        if ((mongolabUri.getUsername() != null) && (mongolabUri.getPassword() != null)) {
-            mongoDB.authenticate(mongolabUri.getUsername(), mongolabUri.getPassword());
+        MongoURI mongoUri = new MongoURI(System.getenv("ORMONGO_URL") != null ? System.getenv("ORMONGO_URL") : "mongodb://127.0.0.1:27017/hello");
+        Mongo m = new Mongo(mongoUri);
+
+        String mongoDb = System.getenv("ORMONGO_DATABASE") != null ? System.getenv("ORMONGO_DATABASE") : mongoUri.getDatabase();
+        mongoDB = m.getDB(mongoDb);
+
+        String mongoUsername = System.getenv("ORMONGO_USERNAME") != null ? System.getenv("ORMONGO_USERNAME") : mongoUri.getUsername();
+        char[] mongoPassword = System.getenv("ORMONGO_PASSWORD") != null ? System.getenv("ORMONGO_PASSWORD").toCharArray() : mongoUri.getPassword();
+
+        if ((mongoUsername != null) && (mongoPassword != null)) {
+            mongoDB.authenticate(mongoUsername, mongoPassword);
         }
 
         contentUrl = System.getenv("CONTENT_URL") != null ? System.getenv("CONTENT_URL") : CONTENT_PATH;
